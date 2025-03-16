19088bef97922b8d49f360f97b63d67fa0a4380a
adtrex <toluadekunte@gmail.com>
2025-03-16 02:17:35 +0000

    fix wallet error

--- submission/functions.sh
+++ submission/functions.sh
@@ -11,12 +11,9 @@
   echo "Network Totals"
   bitcoin-cli -regtest getnettotals
 
+  echo "Creating Wallet"
+  bitcoin_wallet=$(bitcoin-cli -named createwallet wallet_name="adtrex_wallet" descriptors=false)
+  echo "Wallet Info: $(echo $bitcoin_wallet)" 
+
   echo "Wallet Info"
   bitcoin-cli -regtest getwalletinfo
-
-# create_wallet() {
-#   echo "Creating Wallet"
-#   bitcoin_wallet=$(bitcoin-cli -named createwallet wallet_name="adtrex_wallet" descriptors=false)
-#   echo "Wallet Info: $(echo $bitcoin_wallet)" 
-# }
-
